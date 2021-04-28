f6e9cb631512dadd3fc76eed63d3990345fcdd28
Cesar van Hardeveld <cees.van.hardeveld@hva.nl>
2021-04-28 22:06:23 +0200

    Express attempt
    Did not succees

--- matching-app/index.js
+++ matching-app/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+const port = 3000
+app.METHOD(PATH, HANDLER)
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
- 
-app.listen(3000)
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
