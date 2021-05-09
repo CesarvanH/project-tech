14defa7af65e0c526a9637d5149fa649a7140734
Cesar van Hardeveld <cees.van.hardeveld@hva.nl>
2021-05-09 20:24:02 +0200

    Screens en form update

--- matching-app/index.js
+++ matching-app/index.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const exphbs  = require('express-handlebars');
+const mongoose = require('mongoose');
+var bodyParser = require('body-parser')
+const slug = require('slug')
+
 var path = require('path');
 
 const app = express()
@@ -7,25 +11,14 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+app.use(express.static('static'));
 
 app.set('port', (process.env.PORT || 3000));
 
-var people = [
-    {
-        firstName:'Peter',
-        lastName:'Johnson'
-    },
-    {
-        firstName:'John',
-        lastName:'Doe'
-    }
-];
-
 app.get('/', function(req, res){
-    res.render('home',{
-        content: 'This is some content',
+    res.render('login',{
+        content: 'Login',
         published: true,
-        people: people
     });
 });
 
@@ -33,10 +26,40 @@ app.get('/register', function(req, res){
     res.render('register',{
         content: 'Registreer maar',
         published: true,
-        people: people
+    });
+});
+
+app.get('/home', function(req, res){
+    res.render('home',{
+        content: 'Dit is je hoofdscherm',
+        published: true,
+    });
+});
+
+app.get('/results', function(req, res){
+    res.render('result',{
+        content: 'Dit zijn je resultaten',
+        published: true,
     });
 });
 
 app.listen(app.get('port'), function(){
     console.log('Server started on port'+app.get('port'));
 });
+
+app.post('/',add)
+
+function form(req, res) {
+    res.render('register.handlebars')
+}
+
+function add(req, res) {
+    var id = slug(req.body.naam).toLowerCase()
+    
+    data.push({
+        id: id,
+        naam: req.body.naam
+    })
+
+    res.redirect('/' + id)
+}
