79f960b689b7d277f7181cd702a2bb04cf774fe5
Cesar van Hardeveld <cees.van.hardeveld@hva.nl>
2021-05-18 14:37:09 +0200

    Update connect

--- matching-app/models/recipes.js
+++ matching-app/models/recipes.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const recipeSchema = new mongoose.Schema({
     title: String,
     description: String,
-    ingredient: String
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
